c74e318a4c5baa78d2bbdb1893707c3e3275a8fc
litvin <letuwwa@gmail.com>
2026-03-31 20:04:41 +0300

    feat: migration update and base model

--- app/models.py
+++ app/models.py
@@ -1,10 +1,41 @@
+import uuid6
 from .extensions import db
+from datetime import datetime, UTC
+from sqlalchemy.dialects.postgresql.base import UUID
 
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+class BaseModel(db.Model):
+    __abstract__ = True
+
+    id = db.Column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        default=uuid6.uuid7,
+    )
+
+    created_at = db.Column(
+        db.DateTime(timezone=True),
+        default=lambda: datetime.now(UTC),
+        nullable=False,
+    )
+
+    updated_at = db.Column(
+        db.DateTime(timezone=True),
+        default=lambda: datetime.now(UTC),
+        onupdate=lambda: datetime.now(UTC),
+        nullable=False,
+    )
+
+
+class User(BaseModel):
+    __tablename__ = 'users'
+
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
 
-    def __repr__(self):
-        return f"<User {self.email}>"
+
+class Product(BaseModel):
+    __tablename__ = 'products'
+
+    name = db.Column(db.String(255), nullable=False)
+    price = db.Column(db.Numeric(10, 2), nullable=False)
